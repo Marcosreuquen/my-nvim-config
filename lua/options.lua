65c6f1919eca0d6329e7d107ea0ea797aa1a620a
marcos.diaz <marcos.diaz@teladochealth.com>
2025-03-26 16:18:05 -0300

    add folding style

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,18 @@ require "nvchad.options"
 
 -- add yours here!
 
+vim.opt.foldmethod = "expr"               -- Usa una expresión para el plegado
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- Usa Treesitter como método de plegado
+vim.opt.foldenable = true                 -- Activa el plegado por defecto
+vim.opt.foldlevel = 99                    -- Mantiene el código expandido al abrir un archivo
+vim.opt.foldlevelstart = 99
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+
+--function _G.custom_foldtext()
+--        return "+"
+--end
+
+--vim.opt.foldtext = "v:lua.custom_foldtext()"
+vim.cmd([[highlight Folded guifg=#D4A6FF guibg=NONE gui=bold]])  -- Violeta claro sobre fondo oscuro
